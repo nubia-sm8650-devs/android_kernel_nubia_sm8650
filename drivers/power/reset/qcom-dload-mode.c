dc6be8d8919a30ba5f902240e2b13b40302dcd64
Alexander Winkowski <dereference23@outlook.com>
2024-06-29 08:05:45 +0000

    qcom-dload-mode: Disable dump by default
    From NX721J_U(14)_Kernel(6.1.25)_0618.tar.gz
    
    Change-Id: I603672d766959250ef83163708f8ece815ae3031
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/power/reset/qcom-dload-mode.c
+++ drivers/power/reset/qcom-dload-mode.c
@@ -43,6 +43,14 @@ static bool enable_dump =
 	IS_ENABLED(CONFIG_POWER_RESET_QCOM_DOWNLOAD_MODE_DEFAULT);
 static enum qcom_download_mode current_download_mode = QCOM_DOWNLOAD_NODUMP;
 static enum qcom_download_mode dump_mode = QCOM_DOWNLOAD_FULLDUMP;
+#ifdef CONFIG_VENDOR_KERNEL_MODULE_PARAM
+extern bool is_kernel_log_driver_enabled(void);
+#else
+static bool is_kernel_log_driver_enabled(void)
+{
+	return false;
+}
+#endif
 
 static int set_download_mode(enum qcom_download_mode mode)
 {
@@ -338,6 +346,11 @@ static int qcom_dload_probe(struct platform_device *pdev)
 		pr_err("%s: Error in creation sysfs_create_group\n", __func__);
 		kobject_del(&poweroff->kobj);
 		return ret;
+	}
+
+	if (!is_kernel_log_driver_enabled()) {
+		pr_info("%s: disable dump and config hard reset\n", __func__);
+		enable_dump = 0;
 	}
 
 	poweroff->dload_dest_addr = map_prop_mem("qcom,msm-imem-dload-type");
